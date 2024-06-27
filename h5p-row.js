e760009a0c420e7b171fcf7dc7ce6af3595be693
Erik Langhaug <erik.langhaug@h5p.group>
2024-06-27 12:52:34 +0200

    LB-103 Handle resize events bubbling up and down

--- h5p-row.js
+++ h5p-row.js
@@ -21,6 +21,12 @@ H5P.Row = (function (EventDispatcher) {
     // Wrapper element
     let wrapper;
 
+    // Columns in the row
+    const instances = [];
+
+    // Track the containers to be used by each column
+    const instanceContainers = [];
+
     /**
      * Create the HTML for the content type the first time it's attached to
      * the DOM.
@@ -49,8 +55,18 @@ H5P.Row = (function (EventDispatcher) {
           column.style.paddingRight = columnData.paddings.right + unit;
         }
 
-        const h5pRunnable = H5P.newRunnable(columnData.content, rootId);
-        h5pRunnable.attach(H5P.jQuery(column));
+        const instance = H5P.newRunnable(columnData.content, rootId);
+
+        // Bubble resize events received from column
+        bubbleUp(instance, 'resize', self);
+
+        // Keep track of all instances
+        instances.push(instance);
+        instanceContainers.push({
+          hasAttached: false,
+          container: column,
+          instanceIndex: instances.length - 1,
+        });
 
         wrapper.appendChild(column);
       }
@@ -68,15 +84,65 @@ H5P.Row = (function (EventDispatcher) {
         createHTML();
       }
 
+      instanceContainers.filter(function (container) { return !container.hasAttached })
+        .forEach(function (container) {
+          instances[container.instanceIndex]
+            .attach(H5P.jQuery(container.container));
+        });
+
       // Add to DOM
       $container.addClass('h5p-row').html('').append(wrapper);
     };
 
     // TODO: Add required xAPI stuff once we get child instances
+
+    // Resize children to fit inside parent
+    bubbleDown(self, 'resize', instances);
   }
 
   Row.prototype = Object.create(EventDispatcher.prototype);
   Row.prototype.constructor = Row;
+
+  /**
+   * Makes it easy to bubble events from parent to children
+   *
+   * @private
+   * @param {Object} origin Origin of the Event
+   * @param {string} eventName Name of the Event
+   * @param {Array} targets Targets to trigger event on
+   */
+  function bubbleDown(origin, eventName, targets) {
+    origin.on(eventName, function (event) {
+      if (origin.bubblingUpwards) {
+        return; // Prevent send event back down.
+      }
+
+      for (var i = 0; i < targets.length; i++) {
+        targets[i].trigger(eventName, event);
+      }
+    });
+  }
+
+  /**
+   * Makes it easy to bubble events from child to parent
+   *
+   * @private
+   * @param {Object} origin Origin of the Event
+   * @param {string} eventName Name of the Event
+   * @param {Object} target Target to trigger event on
+   */
+  function bubbleUp(origin, eventName, target) {
+    origin.on(eventName, function (event) {
+      // Prevent target from sending event back down
+      target.bubblingUpwards = true;
+
+      // Trigger event
+      target.trigger(eventName, event);
+
+      // Reset
+      target.bubblingUpwards = false;
+    });
+  }
 
   return Row;
 })(H5P.EventDispatcher);
